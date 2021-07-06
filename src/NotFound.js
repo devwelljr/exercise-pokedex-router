cacf2255060317491aefb6a2dca005e4ede8732f
Wellington <junior.wel02@exemplo.br>
2021-07-05 21:36:55 -0300

    requisitos obrigatorios

---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Page not found!</h1>
+        <img src='http://www.quickmeme.com/img/67/673ead76e4494442c513b05417b86e855955da364a870b9f720585b61b593903.jpg' alt='Page not found - Pokemon 404'/>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
